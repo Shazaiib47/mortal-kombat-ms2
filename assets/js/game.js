d5e9288f7ae4e4f5e0c1edaff31fceedd7a2f8b2
Shazaiib47 <shazaib47@hotmail.com>
2020-10-08 17:28:54 +0000

    Added a function that checks for a card match via the dataset that was added in the cards in index.html. Event listeners also added to stop further matching on a non matched pair

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,34 @@ function flipCard() {
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards;
+        return;
+    }
+
+    unflipCards;
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", secondCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard));
