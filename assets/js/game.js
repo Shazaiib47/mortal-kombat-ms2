189b04722318595ef22f8ead639e6e4aab3a5dcb
Shazaiib47 <shazaib47@hotmail.com>
2020-10-13 15:03:05 +0000

    Adding comments to JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,13 @@
 /*jshint esversion: 6 */
 
+// const variables to be declared below that affect the game.
 const cards = document.querySelectorAll(".memory-card");
 const score = document.getElementById("point");
 const finalScore = document.getElementById("finalPoints");
 const won = document.getElementById("won");
 const play = document.getElementById("playAgain");
 
+// point variables to be scripted when user allocates a certain amount of points
 var points = 0;
 var finalPoint = 0;
 var win = 0;
@@ -14,17 +16,20 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+// function to flip the card that also adds the class "flip" to the element. Second part can be found at the bottom.
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
     this.classList.add("flip");
 
+    // below is the matching card logic, the variables hasflippedcard and flippedcard alter the flip state. if there is no flipped card then it is set to true.
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
         return;
     }
 
+    /* below is the code that checks for a match by accessing both the datasets for the firstcard and secondcard. */
     secondCard = this;
     hasFlippedCard = false;
 
@@ -60,12 +65,15 @@ function disableCards() {
 function unflipCards() {
     lockBoard = true;
 
+    /* setTimeout function that is implemented so there is an interval between first card being clicked and the second. 
+    unflipcards will turn both cards back with the 100ms timeout which then removes the flip class */
     setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
         resetBoard();
     }, 1000);
 
+    /* on a unmatch or when no pair is matched, a point gets deducted and goes into the minuses until the user gets a matching pair to get back into receiving points */
     points -= 1;
     finalPoint = points;
     score.innerHTML = points;
@@ -82,6 +90,8 @@ function playAgain() {
 
 play.addEventListener("click", playAgain);
 
+/* a shuffle method that is implemented that takes place every time a new game is commenced.
+    the math.random method returns a random number each time as stated below and this is multiplied by 12 as there are 12 cards in the deck */
 (function shuffle() {
     cards.forEach(card => {
         let ramdomPos = Math.floor(Math.random() * 12);
@@ -89,4 +99,5 @@ play.addEventListener("click", playAgain);
     });
 })();
 
+//added an event listner to cards that flips the card upon the user clicking.
 cards.forEach(card => card.addEventListener("click", flipCard));
